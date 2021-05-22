b03fa1b9ef7dea5fa51b2125581a3450e8015658
yazan shehab <43929918+yazanshehab@users.noreply.github.com>
2021-05-22 20:42:11 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -2,8 +2,8 @@ require("@babel/register");
 require("@babel/polyfill");
 
 module.export ={
-  contracts_directory : './src/contracts/',
-  contracts_build_directory : './src/contracts/output/',
+  contracts_directory : './contracts/',
+  contracts_build_directory : './build/contracts/',
   networks: {
     development: {
       host: "127.0.0.1",
